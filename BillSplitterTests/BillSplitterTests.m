35332df195bbd5045461b4e8a95247a88154308b
LuizRamos <luizramospe@hotmail.com>
2019-05-11 17:08:57 -0400

    First Commit

---
+++ BillSplitterTests/BillSplitterTests.m
@@ -0,0 +1,37 @@
+//
+//  BillSplitterTests.m
+//  BillSplitterTests
+//
+//  Created by Luiz on 5/11/19.
+//  Copyright © 2019 Luiz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BillSplitterTests : XCTestCase
+
+@end
+
+@implementation BillSplitterTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
